363ac4b616323e125e416722c31bd7e1658c2b46
shameed1910 <43462472+shameed1910@users.noreply.github.com>
2018-09-23 22:20:47 +0530

    Add files via upload

---
+++ src/main/java/test/example/demo/TestController.java
@@ -0,0 +1,23 @@
+package test.example.demo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/publish")
+@ResponseBody
+public class TestController
+{
+	@GetMapping("/")
+	public String test()
+	{
+		System.out.println("Hello World");
+		return "Hello World";
+
+	}
+
+}
